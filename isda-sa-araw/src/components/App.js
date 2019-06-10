2870d0228a76c21a1cd93a075b8b36d20b772ff5
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2019-06-10 15:59:07 +0800

    Understanding State

--- isda-sa-araw/src/components/App.js
+++ isda-sa-araw/src/components/App.js
@@ -4,14 +4,29 @@ import Inventory from "./Inventory";
 import Order from "./Order";
 
 class App extends Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  addFish = fish => {
+    // 1. Take a copy of an existing state
+    const fishes = { ...this.state.fishes };
+
+    // 2. Add our new fish to that fishes variable
+    fishes[`fish${Date.now()}`] = fish;
+
+    // 3. Set the new fishes object to state
+    this.setState({ fishes });
+  };
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
         </div>
-        <Inventory />
         <Order />
+        <Inventory addFish={this.addFish} />
       </div>
     );
   }
